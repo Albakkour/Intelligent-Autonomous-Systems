aa2da4cbf8df14cbfe82a81181a140d094986a82
Volker Krueger <volker.krueger@cs.lth.se>
2025-09-12 11:52:11 +0000

    Edit problem_3_table_setting.py

--- technologies/SkiROS2/skiros2_pyrobosim_lib/skiros2_pyrobosim_lib/problem_3_table_setting.py
+++ technologies/SkiROS2/skiros2_pyrobosim_lib/skiros2_pyrobosim_lib/problem_3_table_setting.py
@@ -120,6 +120,8 @@ class problem_3(SkillBase):
                 self.skill("BbUnsetParam", "", remap={"Parameter": "StartLocation"}),
                 self.skill("BbUnsetParam", "", remap={"Parameter": "ObjectStartLocation"}),
                 self.skill("BbUnsetParam", "", remap={"Parameter": "Container"}),
+                self.skill("BbUnsetParam", "", remap={"Parameter": "OpenableLocation"}),
+
             ),
             # FIXME 3.3: Now fetch the butter and close the fridge
             self.skill(SerialStar())(
